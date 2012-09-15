36c11a534307f9af3431c6f86745ae97e3e1733d
Lim Sungguk <limasdf@gmail.com>
2012-09-15 13:11:21 +0900

    Apply i18n locale
    Apply i18n locale

--- youtubeshownext/playlist.js
+++ youtubeshownext/playlist.js
@@ -1,8 +1,39 @@
+﻿// jqeury func
+jQuery.redirect = function(url, params) {
+
+    url = url || window.location.href || '';
+    url =  url.match(/\?/) ? url : url + '?';
+
+    for ( var key in params ) {
+        var re = RegExp( ';?' + key + '=?[^&;]*', 'g' );
+        url = url.replace( re, '');
+        url += '&' + key + '=' + params[key]; 
+    }  
+    // cleanup url 
+    url = url.replace(/[;&]$/, '');
+    url = url.replace(/\?[;&]/, '?'); 
+    url = url.replace(/[;&]{2}/g, ';');
+    // $(location).attr('href', url);
+    window.location.replace( url ); 
+};
+//
+function getParameterByName(name)
+{
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regexS = "[\\?&]" + name + "=([^&#]*)";
+  var regex = new RegExp(regexS);
+  var results = regex.exec(window.location.search);
+  if(results == null)
+    return "";
+  else
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 var getNextVideoUrl = function(){
 	var aryVideoList = document.getElementsByClassName("video-list-item");
-	var videoLink = aryVideoList[3].children[0].href;
+	var randomNo = (Math.random()*3|0) +1;
+	var videoLink = aryVideoList[randomNo].children[0].href;
 	//window.location = videoLink;
-	window.location = videoLink;
+	return videoLink;
 };
 
-var Videos = new Videos();
--- youtubeshownext/replay.js
+++ youtubeshownext/replay.js
@@ -1,4 +1,4 @@
-var intervalTimer;
+﻿var intervalTimer;
 var getPlayerObj = function(){
     var flashPlayerObj = window.document.getElementById("movie_player");
     if(flashPlayerObj){
@@ -26,63 +26,64 @@ var getPlayerObj = function(){
     return null;
 };
 
-var addIcon = function(){
-    var iconTag = document.createElement('IMG');
-    iconTag.src = chrome.extension.getURL("icon2.png");
-    iconTag.setAttribute("style","vertical-align:middle;float:right");
-    document.getElementById("watch-actions").appendChild(iconTag);
-	
-	var likeCountTag = document.createElement('span');
-	likeCountTag.className = "watch-likes-dislikes";
-	likeCountTag.id = "replayCount";
-	likeCountTag.innerHTML = "0" + chrome.i18n.getMessage("TimesAutoReplayed");
-	
-	var tagLocation = document.getElementById("watch-description-extra-info").getElementsByTagName("li")[0];
-	if(tagLocation!=null){
-		document.getElementById("watch-description-extra-info").insertBefore(likeCountTag,tagLocation);	
-	}
-};
-
 var addAdditionalBtn = function(){
 	var shutffleHtml = '<button type="button" class="yt-uix-tooltip yt-uix-tooltip-masked  yt-uix-button yt-uix-button-default yt-uix-button-empty" onclick=";return false;" id="light-shuffle-button" data-button-toggle="true" role="button" data-tooltip-text="Turn shuffle on" title="Turn shuffle on"><span class="yt-uix-button-icon-wrapper"><img class="yt-uix-button-icon yt-uix-button-icon-playlist-bar-shuffle" src="//s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt=""><span class="yt-valign-trick"></span></span></button>';	
 	var autoplayHtml = '<button type="button" class="yt-uix-tooltip yt-uix-tooltip-masked  yt-uix-button yt-uix-button-default yt-uix-button-empty" onclick=";return false;" id="light-autoplay-button" data-button-toggle="true" role="button" data-tooltip-text="Turn autoplay on" title="Turn autoplay on"><span class="yt-uix-button-icon-wrapper"><img class="yt-uix-button-icon yt-uix-button-icon-playlist-bar-autoplay" src="//s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif" alt=""><span class="yt-valign-trick"></span></span></button>';
 	
 	$('#watch-actions').append(autoplayHtml).append(shutffleHtml);
 	
-	$('#light-shuffle-button').click(function(){
-		$('#light-autoplay-button').removeClass('yt-uix-button-toggled');
-	});
-	
 	$('#light-autoplay-button').click(function(){
 		$('#light-shuffle-button').removeClass('yt-uix-button-toggled');
 	});
+	
+	$('#light-shuffle-button').click(function(){
+		$('#light-autoplay-button').removeClass('yt-uix-button-toggled');
+	});
 };
 
-var isLightBtnPressed = function(btnId){
+var isExtensionBtnPressed = function(btnId){
 	var $btn = $('#' + btnId);
 	
 	return $btn.hasClass('yt-uix-button-toggled');
 };
 
+var setInitialButtonStatus = function(){
+	if(getParameterByName('exShuffle') == 1){
+		$('#light-shuffle-button').addClass('yt-uix-button-toggled');
+	}else{
+		$('#light-shuffle-button').removeClass('yt-uix-button-toggled');
+	};
+};
+
+var addInfoDiv = function(){
+	$('#watch-actions').after("<div><b>" + chrome.i18n.getMessage("InfoReplayCount") + " : </b><span id='extensionReplayCntNo'>0</span></div>");
+};
+
+var updateReplayInfo = function(){
+	var $extensionReplayCnt = $('#extensionReplayCntNo');
+	$extensionReplayCnt.text(parseInt($extensionReplayCnt.text())+1);
+};
+
 var pollingCheckAndSeek = function(){
-    //addIcon();
+	addInfoDiv();
 	addAdditionalBtn();
+	setInitialButtonStatus();
     var playerObj = getPlayerObj();
     if(playerObj){
 		intervalTimer = setInterval(function(){
-			if(isLightBtnPressed('light-autoplay-button')){
-				if(playerObj.getPlayerState() == 0){
-				playerObj.seekTo(0, true);
-						// because of Youtube Bug.
-						// registered on Youtube issue tracker
-						playerObj.pauseVideo();
-						playerObj.playVideo();
-						// end of temp code
-				}
-			}else if(isLightBtnPressed('light-shuffle-button')){
-				if(playerObj.getPlayerState() == 0){
+			if(playerObj.getPlayerState() == 0){
+				if(isExtensionBtnPressed('light-autoplay-button')){
+					playerObj.seekTo(0, true);
+					// because of Youtube Bug.
+					// registered on Youtube issue tracker
+					playerObj.pauseVideo();
+					playerObj.playVideo();
+					// end of temp code
+					updateReplayInfo();
+				}else if(isExtensionBtnPressed('light-shuffle-button')){
 					clearInterval(intervalTimer);
-					getNextVideoUrl();
+					//getNextVideoUrl();
+					$.redirect(getNextVideoUrl(), { exShuffle : 1 });
 				}	
 			}
 		}, 250);
